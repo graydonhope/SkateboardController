be38868380ab148e670a6edf1d68b85075229b32
graydonhope <41659296+graydonhope@users.noreply.github.com>
2019-08-21 11:03:41 -0400

    Added Bluetooth lost signal error prevention

--- arduino_bluetooth/arduino_bluetooth.ino
+++ arduino_bluetooth/arduino_bluetooth.ino
@@ -1,7 +1,10 @@
 #include <SoftwareSerial.h>
+#include <Servo.h>
 
 SoftwareSerial mySerial(10, 11); //TX, RX
+Servo esc;
 
+const uint8_t VescOutputPin = 5;
 const int stopLed = 13;
 const int goLed = 12;
 const int throttleLed = 8;
@@ -10,14 +13,15 @@ int mappedSpeed = 0;
 unsigned int btInputValue;
 
 void setup() {
-  // put your setup code here, to run once:
+  // put your setup code here, to run once:  
   pinMode(stopLed, OUTPUT);
   pinMode(throttleLed, OUTPUT);
   pinMode(goLed, OUTPUT);
   pinMode(statusPin, INPUT);
   Serial.begin(9600);
-  
   mySerial.begin(9600);
+
+  esc.attach(VescOutputPin);
 }
 
 void loop() {
@@ -27,9 +31,9 @@ void loop() {
 
   if (statusValue == 0) {
     // Bluetooth Disconnected - Stop Board
-    Serial.print("Status Pin: ");
-    Serial.print(statusValue);
-    Serial.print("\n");
+  //  Serial.print("Status Pin: ");
+  //  Serial.print(statusValue);
+  //  Serial.print("\n");
     stopBoard();
   }
   else {
@@ -37,14 +41,19 @@ void loop() {
       unsigned int btInputA = mySerial.read();
       unsigned int btInputB = mySerial.read();
       btInputValue = (btInputB * 256) + btInputA;
-      Serial.print(btInputValue);
-      Serial.print('\n');
+      
+     // Serial.print(btInputValue);
+     // Serial.print('\n');
       
       if (btInputValue == 2000) {
         stopBoard();
       }
       else if (btInputValue >= 0 && btInputValue <= 255) {
         mappedSpeed = map(btInputValue, 50, 255, 1000, 2000);
+        esc.writeMicroseconds(mappedSpeed);
+        Serial.print("Mapped Speed");
+        Serial.print('\n');
+        Serial.print(mappedSpeed);
         digitalWrite(throttleLed, HIGH);
       }  
       digitalWrite(stopLed, LOW);
@@ -56,7 +65,6 @@ void loop() {
 
 void stopBoard() {
   digitalWrite(stopLed, HIGH);
-  Serial.print(mappedSpeed);
 
   if (mappedSpeed > 1640) {
     Serial.print("Long Stop");
@@ -69,6 +77,10 @@ void stopBoard() {
       }
       else {
         mappedSpeed = newSpeed;
+        esc.writeMicroseconds(mappedSpeed);
+        Serial.print("Mapped Speed");
+        Serial.print('\n');
+        Serial.print(mappedSpeed);
       }
       delay(60);
       Serial.print(mappedSpeed);
@@ -86,6 +98,11 @@ void stopBoard() {
       }
       else {
         mappedSpeed = newSpeed;
+        esc.writeMicroseconds(mappedSpeed);
+
+        Serial.print("Mapped Speed");
+        Serial.print('\n');
+        Serial.print(mappedSpeed);
       }
       delay(85);
       Serial.print(mappedSpeed);
@@ -103,6 +120,11 @@ void stopBoard() {
       }
       else {
         mappedSpeed = newSpeed;
+        esc.writeMicroseconds(mappedSpeed);
+
+        Serial.print("Mapped Speed");
+        Serial.print('\n');
+        Serial.print(mappedSpeed);
       }
       delay(63);
       Serial.print(mappedSpeed); 
